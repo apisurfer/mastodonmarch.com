125fbdc5d27aacc09e752d37a9acc053b0acd86f
Luka Vidakovic <luka.vidakovic@gmail.com>
2023-01-31 22:45:30 +0100

    add site url info

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mastodonmarch.com",
   integrations: [tailwind(), mdx(), sitemap()],
   output: "server",
   adapter: cloudflare(),
